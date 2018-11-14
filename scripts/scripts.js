7ab9c1614885e73486408eea31a66add330c1342
siherrmann <simon.he3@gmail.com>
2018-11-14 16:20:41 +0100

    add responsive dropdown menu with fake links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -96,3 +96,21 @@ function showPicture(n) {
 //     navbar.classList.remove("sticky");
 //   }
 // }
+
+function dropMenu() {
+  document.getElementById("droppedMenuContent").classList.toggle("drop");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropMenu")) {
+    var links = document.getElementsByClassName("menuContent");
+    var i;
+    for (i = 0; i < links.length; i++) {
+      var droppedLinks = links[i];
+      if (droppedLinks.classList.contains("drop")) {
+        droppedLinks.classList.remove("drop");
+      }
+    }
+  }
+};
