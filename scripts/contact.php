8d361bedf404e356a70d8b4216634fdcc9b95c70
siherrmann <Simon.He@gmx.de>
2020-05-26 16:23:47 +0200

    delete all parts using cookies, new map, change text

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,30 +1,58 @@
-if (isset($_POST['send']) && !empty($POST['send'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $nachricht = $_POST['nachricht'];
-  $secretkey = '6LcKjHoUAAAAAMVyhwFE6OuLtXsGqCdEViFgyIma';
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $antwortJSON = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.       $secretkey.'&response='.$_POST['g-recaptcha-response']);
-  $antwortDaten = json_decode($antwortJSON);
+  $lastname = $_POST['lastname'];
 
-  if($antwortDaten->success){
-    mail("info@lachenmaier.org", "$subject - Nachricht von $name", $nachricht, "From: $email");
+  if (strlen($lastname)>0) {
+    http_response_code(403); #403=forbidden
+    echo "Es gab ein internes Problem, bitte versuchen Sie es erneut.";
+    exit;
+  } else {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    // Betreff
-    $betreff = 'Vielen Dank für Ihre Nachricht';
+    if (strpos($email, '@') == false) {
+      http_response_code(403); #403=forbidden
+      echo "Sie müssen eine gültige Email-Adresse eingeben.";
+      exit;
+    }
+    if (strlen($message)<10) {
+      http_response_code(403); #403=forbidden
+      echo "Bitte schreiben Sie eine etwas längere Nachricht.";
+      exit;
+    }
 
-    // Nachricht
-    $nachricht = 'Vielen Dank für Ihre Nachricht, wir werden uns schnellstmöglich bei Ihnen melden.';
+    $mail_to = "info@lachenmaier.org, $email";
+	  $messageResponse_to = '
+  		<html>
+  		<head>
+  			<title>Ihre Nachricht an Lachenmaier Werkzeugbau GmbH</title>
+  		</head>
+  		<body>
+  		  <p>Vielen Dank für Ihre Nachricht, wir werden uns schnellstmöglich bei Ihnen melden.</p>
+  		  <p>Ihre Nachricht an Lachenmaier Werkzeugbau GmbH: '.$message.'</p>
+  		  <p>Mit freundlichen Grüßen,<br/>Ralph Lachenmaier</p>
+  		</body>
+  		</html>';
 
-    <!-- // für HTML-E-Mails muss der 'Content-type'-Header gesetzt werden
-    $header[] = 'MIME-Version: 1.0';
-    $header[] = 'Content-type: text/html; charset=iso-8859-1'; -->
+    $headerResponse_to[] = 'MIME-Version: 1.0';
+    $headerResponse_to[] = 'Content-type: text/html; charset=utf-8';
+	  $headerResponse_to[] = 'From: Lachenmaier Werkzeugbau GmbH <info@lachenmaier.org>';
 
-    // zusätzliche Header
-    $header[] = 'To: $email';
-    $header[] = 'From: Lachenmaier Werkzeugbau GmbH <info@lachenmaier.org>';
-
-    // verschicke die E-Mail
-    mail($email, $betreff, $nachricht, implode("\r\n", $header));
+    $success = mail($mail_to, "$subject - Nachricht von $name", "$messageResponse_to", implode("\r\n", $headerResponse_to));
+    if ($success) {
+        http_response_code(200); #200=okay
+        echo "Danke für Ihre Kontaktaufnahme, $name. Wir werden uns so schnell wie möglich bei Ihnen melden.";
+    } else {
+        http_response_code(500); #500=internal server error
+        echo "Es tut mir leid, aber Ihre Email wurde leider nicht gesendet.";
+        exit;
+    }
   }
+} else {
+  http_response_code(403); #403=forbidden
+  echo "Es gab ein internes Problem, bitte versuchen Sie es erneut.";
 }
+?>
